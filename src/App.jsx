c469245895c1b85cd49ae38d92dd7049997f517e
mohityadav3036 <ymohitayadava@gmail.com>
2025-09-17 01:54:07 +0530

    make a introduction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Signup from './LoginSignup/Signup.jsx'
 import Home from './Home.jsx'
 import Login from './LoginSignup/Login.jsx'
+import IntroductionPage from './Mainpages/IntroductionPage.jsx'
+import Navbar from './Mainpages/Navbar.jsx'
+import TestPage from './Mainpages/TestPage.jsx'
 
 function App() {
+   const [user, setUser] = useState(null);
   return (
     <div>
+      <Navbar user={user}/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup  setUser={setUser}/>} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/" element={<IntroductionPage />} />
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </div>
   )
---
+++ src/Mainpages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TestPage() {
+  return (
+    <div>
+         this is a test page
+    </div>
+  )
+}
+
+export default TestPage
